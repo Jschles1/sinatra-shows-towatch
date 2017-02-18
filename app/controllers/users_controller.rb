2836f659b4f0dd4ec05f1ac7229e2ca42450e808
John Schlesinger <jschles1@ramapo.edu>
2017-02-18 23:22:41 +0000

    Added get /users/:slug & get /signup routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
+  end
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      flash[:message] = "You are already logged in. Redirecting to your list of shows."
+      redirect to '/users/:slug'
+    end
+  end
 
 end
