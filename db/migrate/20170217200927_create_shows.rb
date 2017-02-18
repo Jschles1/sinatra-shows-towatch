813b658f20a4f4b58d5c58e36d8a93c58385bfc7
John Schlesinger <jschles1@ramapo.edu>
2017-02-18 01:57:59 +0000

    Defined change methods

--- db/migrate/20170217200927_create_shows.rb
+++ db/migrate/20170217200927_create_shows.rb
@@ -1,4 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.0]
   def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :showtime
+      t.integer :user_id
+    end
   end
 end
