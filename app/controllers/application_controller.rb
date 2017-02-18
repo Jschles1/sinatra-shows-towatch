d846fef5ce40ebdf746ed18a837696cb597bc9c8
John Schlesinger <jschles1@ramapo.edu>
2017-02-18 02:18:17 +0000

    Added configure method & required Rack::Flash

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < Sinatra::Base
+  use Rack::Flash
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "shows secret"
+  end
+  
+end
