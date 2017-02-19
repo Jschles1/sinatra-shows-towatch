4c41fbde3bd06af5cc5d39049d4faa8deb4188d2
John Schlesinger <jschles1@ramapo.edu>
2017-02-19 01:18:55 +0000

    Defined get /shows/new route

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,8 +3,17 @@ class ShowsController < ApplicationController
   get '/shows' do
     if logged_in?
       @user = User.find_by_id(session[:user_id])
-      @shows = Show.all
+      @shows = Show.all #Find by id current_user
       erb :'/shows/shows'
+    else
+      flash[:message] = "You must log in to access this page."
+      redirect to '/login'
+    end
+  end
+
+  get '/shows/new' do
+    if logged_in?
+      erb :'/shows/create_show'
     else
       flash[:message] = "You must log in to access this page."
       redirect to '/login'
