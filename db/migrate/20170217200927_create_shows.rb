55f921980413752f6b3b1f731385c84855c8912d
John Schlesinger <jschles1@ramapo.edu>
2017-02-20 03:10:54 +0000

    Added weekday column

--- db/migrate/20170217200927_create_shows.rb
+++ db/migrate/20170217200927_create_shows.rb
@@ -3,6 +3,7 @@ class CreateShows < ActiveRecord::Migration[5.0]
     create_table :shows do |t|
       t.string :name
       t.string :network
+      t.string :weekday
       t.string :showtime
       t.integer :user_id
     end
