76be2e72f7ba6f9f34edbe201775482611056110
John Schlesinger <jschles1@ramapo.edu>
2017-02-28 00:08:43 +0000

    Removed comment

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -3,7 +3,7 @@ class ShowsController < ApplicationController
   get '/shows' do
     if logged_in?
       @user = User.find_by_id(session[:user_id])
-      @shows = @user.shows #Show.all.find{|show| current_user.id == show.user_id }#Find where current_user.id == show.user_id or cuurent_user.shows
+      @shows = @user.shows 
       erb :'/shows/shows'
     else
       flash[:message] = "You must log in to access this page."
