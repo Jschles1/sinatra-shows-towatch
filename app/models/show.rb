31ffd3dab25c0f2e7d81560fe44d6cabd6b4b070
John Schlesinger <jschles1@ramapo.edu>
2017-03-03 01:28:06 +0000

    Added validations

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,8 @@
 class Show < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, presence: true
+  validates :network, presence: true
+  validates :weekday, presence: true
+  validates :showtime, presence: true
 end
-  
